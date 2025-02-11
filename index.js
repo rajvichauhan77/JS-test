c930210014c2472464c9ac50a5764028e50581e2
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-02-11 18:23:59 +0530

    asf

---
+++ index.js
@@ -0,0 +1,21 @@
+let login = JSON.parse(localStorage.getItem("objlog"))
+
+getmarks(login)
+
+function getmarks(ele){
+
+    let card = document.getElementById("card")
+    card.innerHTML = `
+
+     <div class="card-body">
+              <h5 class="card-title">Score board</h5>
+              <p class="card-text"><h5>Name : </h5>${ele.firstname}</p>
+              <p class="card-text"><h5>Email : </h5>${ele.email}</p>
+              <p class="card-text"><h5>Subject : </h5>${ele.Subject}</p>
+              <p class="card-text"><h5>Marks : </h5>${ele.Marks}</p>
+              
+              
+    </div>
+    `
+
+}
